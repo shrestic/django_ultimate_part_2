229a66dab8b4e2c48bcf8c39abbdf19b76f75b75
shrestic <nhatphongcgp@gmail.com>
2025-03-02 11:29:53 +0700

    Generic Filtering

--- store/views.py
+++ store/views.py
@@ -1,4 +1,6 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from django.shortcuts import get_object_or_404
+from store.filters import ProductFilter
 from store.serializers import ProductSerializer, CollectionSerializer, ReviewSerializer
 from .models import OrderItem, Product, Collection, Review
 from django.db.models import Count
@@ -11,19 +13,8 @@ from rest_framework.viewsets import ModelViewSet
 class ProductViewSet(ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
-    def get_queryset(self):
-        queryset = Product.objects.all()
-        # MultiValueDictKeyError at /store/products/
-        # 'collection_id'
-        # You're encountering a MultiValueDictKeyError because "collection_id" might not always be present in self.request.query_params
-        collection_id = self.request.query_params.get("collection_id")
-        if collection_id is not None:
-            queryset = queryset.filter(collection_id=collection_id)
-        return queryset
-
-        # This code may not work if we dont have collection_id
-        # return Product.objects.filter(collection_id=self.request.content_params["collection_id"])
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = ProductFilter
 
     def get_serializer_context(self):
         return {"request": self.request}
